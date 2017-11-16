802de99f4c374ef0fa8387e3f92a18882d68b64f
Anton Todua <toduaanton@yandex.ru>
2017-11-17 00:18:19 +0300

    disable slow output

--- src/svn.cpp
+++ src/svn.cpp
@@ -349,8 +349,8 @@ static int recursiveDumpDir(Repository::Transaction *txn, svn_fs_root_t *fs_root
             if (recursiveDumpDir(txn, fs_root, entryName, entryFinalName, dirpool, revnum, rule, matchRules, ruledebug) == EXIT_FAILURE)
                 return EXIT_FAILURE;
         } else if (i.value() == svn_node_file) {
-            printf("+");
-            fflush(stdout);
+            // printf("+");
+            // fflush(stdout);
             if (dumpBlob(txn, fs_root, entryName, entryFinalName, dirpool) == EXIT_FAILURE)
                 return EXIT_FAILURE;
         }
@@ -760,8 +760,8 @@ int SvnRevision::exportInternal(const char *key, const svn_fs_path_change2_t *ch
         return EXIT_FAILURE;
     }
 
-    printf(".");
-    fflush(stdout);
+    // printf(".");
+    // fflush(stdout);
 //                qDebug() << "   " << qPrintable(current) << "rev" << revnum << "->"
 //                         << qPrintable(repository) << qPrintable(branch) << qPrintable(path);
 
